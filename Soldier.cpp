ff89a34935751e67252813c2554b99da9013032b
dekel241 <dekel241@gmail.com>
2020-06-02 03:56:17 +0300

    Redesign Soldier class

--- Soldier.cpp
+++ Soldier.cpp
@@ -4,7 +4,6 @@
 
 #include <cmath>
 #include "Soldier.hpp"
-#include <stdexcept>
 
 namespace WarGame {
 
@@ -16,7 +15,7 @@ namespace WarGame {
                     pow(y2 - y1, 2) * 1.0);
     }
 
-    pair<int,int> Soldier::find_closest_enemy(vector<vector<Soldier*>> & board)
+    pair<int,int> Soldier::find_closest_enemy(pair<int, int> src, vector<vector<Soldier*>> & board)
     {
         double min_distance = numeric_limits<double>::max();//Assign MAX value
         pair<int,int> result(-1,-1);//mark it as unassigned
@@ -24,11 +23,10 @@ namespace WarGame {
         for(int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[i].size(); j++) {
                 if (board[i][j] != nullptr && board[i][j]->getPlayer() != _player) {
-                    double dis = distance(_loc.first, _loc.second, board[i][j]->getLoc().first,
-                                          board[i][j]->getLoc().second);
+                    double dis = distance(src.first, src.second, i, j);
                     if (dis < min_distance) {
                         min_distance = dis;
-                        result = board[i][j]->getLoc();
+                        result = {i, j};
                     }
                 }
             }
@@ -48,7 +46,7 @@ namespace WarGame {
                     double hp = board[i][j]->getHp();
                     if (hp > max_hp) {
                         max_hp = hp;
-                        result = board[i][j]->getLoc();
+                        result = {i, j};
                     }
                 }
             }
@@ -57,35 +55,16 @@ namespace WarGame {
         return result;
     }
 
-    void Soldier::move(MoveDIR dir, vector<vector<Soldier *>> &board) {
-        switch (dir)
-        {
-            case Up:
-            {
-                _loc.first +=1;//first = x, second = y
-                break;
-            }
-            case Down:
-            {
-                _loc.first -=1;
-                break;
-            }
-            case Right:
-            {
-                _loc.second +=1;
-                break;
-            }
-            case Left:
-            {
-                _loc.second -=1;
-                break;
-            }
-            default:
-            {
-                throw invalid_argument("Unknown direction");
-            }
-        }
+    void Soldier::move(pair<int, int> src, MoveDIR dir, vector<vector<Soldier *>> &board) {
+        act(src, board);
+    }
 
-        act(board);
+    void Soldier::damage(const int amount, pair<int,int> loc, vector<vector<Soldier *>> &board) {
+        _hp -= amount;
+        if(getHp() <= 0)
+        {
+            delete this;
+            board[loc.first][loc.second] = nullptr;
+        }
     }
 }
--- Soldier.hpp
+++ Soldier.hpp
@@ -10,43 +10,71 @@
 #include "MoveDIR.hpp"
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <iostream>
+
 
 using namespace std;
 
 namespace WarGame {
     class Soldier{
-
         double distance(int x1, int y1, int x2, int y2);
+
     protected:
         int _player;//1 or 2
         int _hp;
-        pair<int,int> _loc;
         string _name;
 
         Soldier(int player, int hp, string name): _player(player), _hp(hp), _name(name){};
 
-        pair<int,int> find_closest_enemy(vector<vector<Soldier*>> & board);
+        pair<int,int> find_closest_enemy(pair<int, int> src, vector<vector<Soldier*>> & board);
         pair<int,int> find_strongest_enemy(vector<vector<Soldier*>> & board);
+
+        virtual void act(pair<int, int> src, vector<vector<Soldier*>> & board) = 0;
+
+        template <class T>
+        void actuate_subordinate(vector<vector<Soldier *>> &board) {
+
+            for(int i = 0; i < board.size(); i++) {
+                for (int j = 0; j < board[i].size(); j++) {
+                    T * subordinate = dynamic_cast<T*>(board[i][j]);
+                    //Check it actually FootSoldier and they are on the same team
+                    if (subordinate && subordinate->getPlayer() == _player) {
+                        subordinate->act({i, j}, board);//Make him attack the enemy as well !
+                    }
+                }
+            }
+        }
+
     public:
-        virtual void act(vector<vector<Soldier*>> & board) = 0;
-        void move(MoveDIR dir, vector<vector<Soldier*>> & board);
+        virtual ~Soldier(){}//We want to call delete of the correct object
+        void move(pair<int, int> src, MoveDIR dir, vector<vector<Soldier*>> & board);
 
         int getPlayer() const {return _player;}
         int getHp() const {return _hp;}
         const string getName() const {return _name;}
-        const pair<int,int> &getLoc() const {return _loc;}
-        void damage(int amount){_hp -= amount;}
+        void damage(const int amount, pair<int,int> loc, vector<vector<Soldier *>> &board);
 
-        // Move Assignment Operator
-        Soldier & operator=(Soldier&& other)
-        {
-            _player = exchange(other._player, 0);
-            _hp = exchange(other._hp, 0);
-            _loc = exchange(other._loc, nullptr);
-            _name = exchange(other._name, nullptr);
-            exchange(other, nullptr);//TODO:Check it's working !
-            return *this;
-        }
+        // Move assignment operator
+//        Soldier & operator=(Soldier & other)
+//        {
+//            if(this != &other) {
+//                _player = exchange(other._player, 0);
+//                _hp = exchange(other._hp, 0);
+//                _name = std::move(other._name);
+//            }
+//            return *this;
+//        }
+//        //Copy assignment operator
+//        Soldier & operator=(Soldier & other)
+//        {
+//            cout << "Inside assignment operator" << endl;
+//            std::swap(_player, other._player);
+//            std::swap(_hp, other._hp);
+//            std::swap(_name, other._name);
+//
+//            return *this;
+//        }
     };
 }
 
