7fdf56d6a8b1a7b2354dd2def5d607bb00ac34a5
dekel241 <dekel241@gmail.com>
2020-06-03 04:18:55 +0300

    Fix bug in paramdeic heal_teammates

--- Paramedic.cpp
+++ Paramedic.cpp
@@ -11,11 +11,6 @@ namespace WarGame{
         vector<vector<Soldier *>> helper_board(board.size() + 2, std::vector<Soldier*>(board[0].size() + 2, nullptr));
 
         //Copy small board into bigger temp board
-//        for(int i = src.first-1; i < src.first + 2; i++){
-//            for(int j = src.second - 1; j < src.second + 2; j++){
-//                helper_board[i][j] = board[i][j];
-//            }
-//        }
 
         for(int i = 1; i < helper_board.size() - 1; i++)
         {
@@ -25,18 +20,18 @@ namespace WarGame{
             }
         }
 
+        pair<int,int> helper_src = {src.first + 1, src.second + 1};
         //Now we can easily compare and heal without check specifically all boundaries.
-        for(int i = src.first-1; i <  src.first + 2; i++){
-            for(int j = src.second - 1; j < src.second + 2; j++){
+        for(int i = helper_src.first-1; i <  helper_src.first + 2; i++){
+            for(int j = helper_src.second - 1; j < helper_src.second + 2; j++){
                 //If there is a soldier and he is on my team and it's not me - then heal !
-                if(helper_board[i][j] && board[src.first][src.second]->getPlayer() == helper_board[i][j]->getPlayer() &&
-                        (i-1 != src.first || j-1 != (src.second)))
+                if(helper_board[i][j] && helper_board[helper_src.first][helper_src.second]->getPlayer() == helper_board[i][j]->getPlayer() &&
+                        (i != helper_src.first || j != helper_src.second))
                 {
                     helper_board[i][j]->heal();
                 }
             }
         }
-
     }
 
     void Paramedic::act(pair<int, int> src, vector<vector<Soldier *>> &board) {
