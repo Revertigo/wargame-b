f260fec9ee41415f94040034f70faae061c1458f
dekel241 <dekel241@gmail.com>
2020-06-04 01:24:49 +0300

    Fix bugs in move() according to tests

--- Board.cpp
+++ Board.cpp
@@ -4,7 +4,6 @@
 
 #include "Board.hpp"
 #include <stdexcept>
-#include "MoveDIR.hpp"
 #include <iostream>
 
 using namespace std;
@@ -17,8 +16,13 @@ namespace WarGame {
         return board[location.first][location.second];
     }
 
-    void Board::move(uint player_number, std::pair<int,int> source, WarGame::MoveDIR direction)
+    void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction)
     {
+        if(source.first >= board.size() || source.first <= -1 || source.second >= board[0].size() || source.second <= -1)
+        {
+            throw invalid_argument("source is out board boundries");
+        }
+
         Soldier * soldier = board[source.first][source.second];
         std::pair<int,int> updated_source;
 
@@ -34,8 +38,8 @@ namespace WarGame {
         switch(direction)
         {
             case Up: {
-                if (board[source.first + 1][source.second]) {
-                    throw invalid_argument("The target location is already populated");
+                if (source.first + 1 >= board.size() || board[source.first + 1][source.second]) {
+                    throw invalid_argument("The target location is either out of board or already populated - UP");
                 }
 
                 board[source.first + 1][source.second] = soldier;
@@ -43,8 +47,8 @@ namespace WarGame {
                 break;
             }
             case Down: {
-                if (board[source.first - 1][source.second]) {
-                    throw invalid_argument("The target location is already populated");
+                if (source.first - 1 <= -1  || board[source.first - 1][source.second]) {
+                    throw invalid_argument("TThe target location is either out of board or already populated - DOWN");
                 }
 
                 board[source.first - 1][source.second] = std::move(soldier);//Triggers the move assignment operator
@@ -52,8 +56,8 @@ namespace WarGame {
                 break;
             }
             case Right: {
-                if (board[source.first][source.second + 1]) {
-                    throw invalid_argument("The target location is already populated");
+                if (source.second +1 >= board[source.first].size() || board[source.first][source.second + 1]) {
+                    throw invalid_argument("The target location is either out of board or already populated - RIGHT");
                 }
 
                 board[source.first][source.second + 1] = std::move(soldier);//Triggers the move assignment operator
@@ -61,8 +65,8 @@ namespace WarGame {
                 break;
             }
             case Left: {
-                if (board[source.first][source.second - 1]) {
-                    throw invalid_argument("The target location is already populated");
+                if (source.second -1 <= -1 || board[source.first][source.second - 1]) {
+                    throw invalid_argument("The target location is either out of board or already populated - LEFT");
                 }
 
                 board[source.first][source.second - 1] = std::move(soldier);//Triggers the move assignment operator
@@ -76,7 +80,7 @@ namespace WarGame {
         }
         board[source.first][source.second] = nullptr;//set previous location to nullptr(mark as free location)
 
-        soldier->move(updated_source, direction, board);
+        soldier->move(updated_source, board);
     }
 
     //Assuming addition of new troops is only at the beginning of the game
